21c038a8580f7cea431838ff0769166f359b3492
unknown <bigrocks918>
2024-01-15 18:12:48 +0900

    new

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/mobile', {
+  .connect('mongodb://https://relieved-crab-snaps.cyclic.app:27017/mobile', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,

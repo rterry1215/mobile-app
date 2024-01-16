5730a33b551d4edf530fffc33314c956024e19a2
unknown <bigrocks918>
2024-01-16 14:32:32 +0900

    make fake path

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require("express");
 require("dotenv").config();
 require("./config/db");
 const userRouter = require("./routes/user");
-const morgan = require('morgan');
+const morgan = require("morgan");
+const user = require("./models/user");
 
 const app = express();
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(userRouter);
+
+app.get("/info", async (req, res) => {
+  try {
+    const users = await user.find({});
+    res.json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
+});
 // testing
 // app.get('/', (req, res) => {
 //   res.json({ success: true, message: 'Welcome to backend zone!' });
